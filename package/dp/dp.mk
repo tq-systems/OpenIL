fca42a639373cbb08a03be82ea30bf2f26ccdd79
Gregor Herburger <gregor.herburger@tq-group.com>
2019-07-24 13:03:58 +0200

    dp.mk: use buildroot download functions to source files
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

--- package/dp/dp.mk
+++ package/dp/dp.mk
@@ -4,11 +4,18 @@
 #
 ################################################################################
 
+DP_SITE = https://www.nxp.com/lgfiles/sdk/ls1028a_bsp_01
+DP_SOURCE = ls1028a-dp-fw.bin
 DP_BIN = $(call qstrip,$(BR2_PACKAGE_DP_BIN))
- 
+
+define DP_EXTRACT_CMDS
+	cp $(BR2_DL_DIR)/$(DP_SOURCE) $(@D)/ &&\
+	cd $(@D)/ && \
+	chmod +x $(DP_SOURCE) && ./$(DP_SOURCE) --auto-accept;
+endef
+
 define DP_BUILD_CMDS
-	cd $(@D)/ && wget https://www.nxp.com/lgfiles/sdk/ls1028a_bsp_01/ls1028a-dp-fw.bin &&\
-	chmod +x ls1028a-dp-fw.bin && ./ls1028a-dp-fw.bin --auto-accept && \
+	cd $(@D)/ && \
 	cp -f ls1028a-dp-fw/cadence/$(DP_BIN) $(BINARIES_DIR)/ls1028a-dp-fw.bin;
 endef
 
